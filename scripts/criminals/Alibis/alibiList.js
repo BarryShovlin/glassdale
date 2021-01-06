58396faddadea9f368e0fc290b11e90759faacb7
BarryShovlin <bshovlin32@gmail.com>
2021-01-06 13:26:03 -0600

    can now save notes using criminal id with selected criminal from a select element.  Can now delete notes

--- scripts/criminals/Alibis/alibiList.js
+++ scripts/criminals/Alibis/alibiList.js
@@ -1,29 +1,56 @@
 import { getCriminals, useCriminals } from "../criminalProvider.js"
 
-
-const contentTarget = document.querySelector(".associate_alibi")
 const eventHub = document.querySelector(".container")
 
 
 eventHub.addEventListener("alibiButtonClicked", event => {
-   if (event.detail.criminalThatWasCosen !== 0) {
    
     const criminals = useCriminals()
     const chosenCriminal = criminals.find( (criminal) => criminal.id === parseInt(event.detail.criminalThatWasChosen))
  
-    contentTarget.innerHTML = `
-    <h3 class="crimName">Known Associates of ${chosenCriminal.name}</h3>
-    ${chosenCriminal.known_associates.map(buddy => `
-    <h4 class=associate_name>${buddy.name}</h4>
-    <h3 class="alibi>Alibi: ${buddy.alibi}</h3>
-    `).join("")
-   
-   }`
-}
+    openDialog(chosenCriminal.known_associates)
 }
 )
 
+eventHub.addEventListener("click", event => {
+    if (event.target.id === "close-associate-dialog") {
+        closeDialog()
+    }
+})
+
+const buddyDialog = (buddy) => {
+    return `
+      <section class="associate-dialog">
+        <div class="associate-dialog__body">
+          <h1>Known Associates</h1>
+          <div class="associate-dialog__list">
+            ${buddy.map(bud => buddyCard(bud)).join("")}
+          </div>
+            <button id="close-associate-dialog">Close</button>
+        </div>
+      </section>
+    `;
+  }
+
+const openDialog = (associates) => {
+    const ContentElement = document.querySelector('.criminalsContainer');
+    ContentElement.innerHTML = buddyDialog(associates);
+  }
+  
+  const closeDialog = () => {
+    const contentTarget = document.querySelector('.associate_alibi');
+    contentTarget.innerHTML = "";
+  };
 
 
 
+
+  const buddyCard = (budd) => {
+    return `
+      <div class="associate-dialog__card">
+        <h2 class="associate-dialog__name"> ${budd.name} </h2>
+        <h4 class="associate-dialog__alibi">Alibi: ${budd.alibi} </h4>
+      </div>
+    `;
+  };
   
--- scripts/notes/noteProvider.js
+++ scripts/notes/noteProvider.js
@@ -22,13 +22,22 @@ export const getNotes = () => {
 }
 
 export const saveNote = note => {
+    let stringifyObj = JSON.stringify(note)
     return fetch('http://localhost:8088/notes', {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(note)
+        body: stringifyObj
     })
     .then(getNotes)
     .then(dispatchStateChangeEvent)
 }
+
+export const deleteNote = noteId => {
+    return fetch(`http://localhost:8088/notes/${noteId}`, {
+        method: "DELETE"
+    })
+    .then(getNotes)
+    .then(dispatchStateChangeEvent)
+  }
